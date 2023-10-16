e32e72241da0a06b9a2701e74cb73555fa239123
maxogod <maxi18wars@gmail.com>
2023-10-16 20:37:07 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Home from "./components/Home"
+import Navbar from "./shared/Navbar"
 
 function App() {
 
-  return <h1>init</h1>
+  return (
+    <div className="bg-blue-300">
+      <Navbar />
+      <Home />
+    </div>
+  )
 }
 
 export default App
---
+++ src/shared/Navbar.tsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div className="h-12 flex justify-end items-center px-5 py-1">
+            <button className="bg-white h-full">Burger</button>
+        </div>
+    )
+}
+
+export default Navbar
